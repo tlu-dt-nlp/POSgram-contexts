c87dc1a9f119d5dd747e03605275ab031bc25c2d
Jaagup Kippar <jaagup@tlu.ee>
2024-01-22 15:25:31 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
-from POSgramContexts import POSgramContexts
+from posgram_contexts import PosgramContexts
 
-p=POSgramContexts("""Juku tuli kooli. Juku tuli kooli ruttu. Unine Juku tuli kooli.""")
+p=PosgramContexts("""Juku tuli kooli. Juku tuli kooli ruttu. Unine Juku tuli kooli.""")
 print(p.precontexts())        
 p.precontexts("juku_pre.json")
 p.postcontexts("juku_post.json")
