ccea5d8c405f996ef648669a80a54e3676e045a3
Kais Allkivi-Metsoja <71447859+kaisall@users.noreply.github.com>
2024-01-31 00:06:12 +0200

    Update example.py

--- example.py
+++ example.py
@@ -2,6 +2,12 @@ from posgram_contexts import PosgramContexts
 
 p = PosgramContexts("""Juku tuli kooli. Juku tuli kooli ruttu. Unine Juku tuli kooli.""")
 print(p.precontexts())
-print(p.postcontexts())  
-p.precontexts("juku_pre.json")
-p.postcontexts("juku_post.json")
+print(p.postcontexts())
+
+#Input from file. 
+#The example text file contains the L1 reference subcorpus of the Estonian Interlanguage Corpus.
+#This subcorpus consists of opinion articles published in Postimees and Õhtuleht in 2014.
+#See https://elle.tlu.ee/tools/.
+p = PosgramContexts(open("EIC_L1_reference.txt", "r", encoding="utf-8").read())
+p.precontexts("EIC_L1_pre.json")
+p.postcontexts("EIC_L1_post.json")
